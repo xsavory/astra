8112ef4f5c14f55d9ef4ef0264cd4ec098043c25
Danny <dnmiloen@gmail.com>
2025-11-02 01:30:47 +0700

    feat: add style into page loader

--- apps/expert-forum-2025/src/components/page-loader.tsx
+++ apps/expert-forum-2025/src/components/page-loader.tsx
@@ -1,6 +1,10 @@
+import { Loader2 } from 'lucide-react'
+
 function PageLoader() {
   return (
-    <div>Loading</div>
+    <div className="flex min-h-screen items-center justify-center">
+      <Loader2 className="size-8 animate-spin text-muted-foreground" />
+    </div>
   )
 }
 
